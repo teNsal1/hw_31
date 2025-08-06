cb478c746942c970503a4646ae0eb1200b473195
Багаутдинов-Ринат <tensal@bk.ru>
2025-08-06 14:16:21 +0300

    hw_31: Настройка URL-маршрутов

--- barbershop_project/users/urls.py
+++ barbershop_project/users/urls.py
@@ -1,8 +1,33 @@
 from django.urls import path
-from .views import RegisterView, UserLoginView, UserLogoutView
+from .views import (
+    UserRegisterView,
+    UserLoginView,
+    UserLogoutView,
+    UserProfileDetailView,
+    UserProfileUpdateView,
+    UserPasswordChangeView,
+    CustomPasswordResetView,
+    CustomPasswordResetDoneView,
+    CustomPasswordResetConfirmView,
+    CustomPasswordResetCompleteView
+)
 
 urlpatterns = [
-    path('register/', RegisterView.as_view(), name='register'),
+    # Аутентификация
+    path('register/', UserRegisterView.as_view(), name='register'),
     path('login/', UserLoginView.as_view(), name='login'),
     path('logout/', UserLogoutView.as_view(), name='logout'),
+    
+    # Профиль пользователя
+    path('profile/', UserProfileDetailView.as_view(), name='profile_detail'),
+    path('profile/edit/', UserProfileUpdateView.as_view(), name='profile_edit'),
+    
+    # Смена пароля
+    path('password-change/', UserPasswordChangeView.as_view(), name='password_change'),
+    
+    # Восстановление пароля
+    path('password-reset/', CustomPasswordResetView.as_view(), name='password_reset'),
+    path('password-reset/done/', CustomPasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', CustomPasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', CustomPasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
