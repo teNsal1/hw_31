657f9ab31f1cdccf867662766760a68b43329e6d
Багаутдинов-Ринат <tensal@bk.ru>
2025-08-06 14:15:13 +0300

    hw_31: Создание представлений (views) для обработки форм

--- barbershop_project/users/views.py
+++ barbershop_project/users/views.py
@@ -1,33 +1,112 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth import login, logout
-from django.contrib.auth.views import LoginView as BaseLoginView
-from django.views.generic import CreateView, View
+from django.shortcuts import redirect
 from django.urls import reverse_lazy
-from .forms import UserRegisterForm, UserLoginForm
+from django.views.generic import (
+    CreateView, 
+    DetailView, 
+    UpdateView,
+    View
+)
+from django.contrib.auth.views import (
+    LoginView,
+    LogoutView,
+    PasswordChangeView,
+    PasswordResetView,
+    PasswordResetDoneView,
+    PasswordResetConfirmView,
+    PasswordResetCompleteView
+)
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth import login
+from django.contrib import messages
+from django.contrib.auth.models import User
+from .forms import (
+    UserRegisterForm,
+    UserLoginForm,
+    UserProfileUpdateForm,
+    UserPasswordChangeForm,
+    CustomPasswordResetForm,
+    CustomSetPasswordForm
+)
 
-class RegisterView(CreateView):
-    """Обработка регистрации новых пользователей"""
+class UserRegisterView(CreateView):
     form_class = UserRegisterForm
     template_name = 'users/register.html'
-    success_url = reverse_lazy('landing')
+    success_url = reverse_lazy('profile_detail')
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('profile_detail')
+        return super().dispatch(request, *args, **kwargs)
 
     def form_valid(self, form):
         response = super().form_valid(form)
         user = form.save()
         login(self.request, user)
+        messages.success(self.request, 'Вы успешно зарегистрировались!')
         return response
 
-class UserLoginView(BaseLoginView):
-    """Обработка аутентификации пользователей"""
+class UserLoginView(LoginView):
     form_class = UserLoginForm
     template_name = 'users/login.html'
     redirect_authenticated_user = True
 
-class UserLogoutView(View):
-    def get(self, request):
-        logout(request)
-        return render(request, 'users/logout.html')
-    
-    def post(self, request):
-        logout(request)
-        return render(request, 'users/logout.html')
+    def form_valid(self, form):
+        messages.success(self.request, 'Вы успешно вошли в систему!')
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        messages.error(self.request, 'Ошибка входа. Проверьте данные.')
+        return super().form_invalid(form)
+
+class UserLogoutView(LogoutView):
+    next_page = reverse_lazy('landing')
+
+    def dispatch(self, request, *args, **kwargs):
+        messages.success(request, 'Вы успешно вышли из системы!')
+        return super().dispatch(request, *args, **kwargs)
+
+class UserProfileDetailView(LoginRequiredMixin, DetailView):
+    model = User
+    template_name = 'users/profile_detail.html'
+    context_object_name = 'user_profile'
+
+    def get_object(self, queryset=None):
+        return self.request.user
+
+class UserProfileUpdateView(LoginRequiredMixin, UpdateView):
+    form_class = UserProfileUpdateForm
+    template_name = 'users/profile_update_form.html'
+    success_url = reverse_lazy('profile_detail')
+
+    def get_object(self, queryset=None):
+        return self.request.user
+
+    def form_valid(self, form):
+        messages.success(self.request, 'Профиль успешно обновлен!')
+        return super().form_valid(form)
+
+class UserPasswordChangeView(LoginRequiredMixin, PasswordChangeView):
+    form_class = UserPasswordChangeForm
+    template_name = 'users/password_change_form.html'
+    success_url = reverse_lazy('profile_detail')
+
+    def form_valid(self, form):
+        messages.success(self.request, 'Пароль успешно изменен!')
+        return super().form_valid(form)
+
+class CustomPasswordResetView(PasswordResetView):
+    form_class = CustomPasswordResetForm
+    template_name = 'users/password_reset_form.html'
+    email_template_name = 'users/password_reset_email.html'
+    success_url = reverse_lazy('password_reset_done')
+
+class CustomPasswordResetDoneView(PasswordResetDoneView):
+    template_name = 'users/password_reset_done.html'
+
+class CustomPasswordResetConfirmView(PasswordResetConfirmView):
+    form_class = CustomSetPasswordForm
+    template_name = 'users/password_reset_confirm.html'
+    success_url = reverse_lazy('password_reset_complete')
+
+class CustomPasswordResetCompleteView(PasswordResetCompleteView):
+    template_name = 'users/password_reset_complete.html'
